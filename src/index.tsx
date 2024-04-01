31d270875f9fb046ab60d7ace017f4218c568e6e
Prateek <prateekreddyy@gmail.com>
2024-04-01 16:30:09 +0400

    cleanup: remove Estonia id related files

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./app";
-
-window.onload = () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-};
